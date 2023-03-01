80a42e5b7b4a3f88626d46b0a2a0006eac3a14cb
Fireayehu Zekarias <fireayehuzekarias@gmail.com>
2023-03-02 00:41:25 +0300

    feat: refactor item card component

--- src/components/item-card/item-card.tsx
+++ src/components/item-card/item-card.tsx
@@ -5,6 +5,7 @@ import { useStyles } from './styles';
 import { IItemCardProps } from './type';
 import { useItem } from 'hooks/item';
 import { DatePicker } from 'components/date-picker';
+import { IAttribute } from 'store/category/type';
 
 export const ItemCard: React.FC<IItemCardProps> = ({
   item,
@@ -25,64 +26,70 @@ export const ItemCard: React.FC<IItemCardProps> = ({
     return null;
   }, [item, titleField]);
 
+  const renderTextAttribute = (attribute: IAttribute) => {
+    const value =
+      item.values.find(value => value.attribute === attribute.id)?.value || '';
+    return (
+      <TextInput
+        mode="outlined"
+        label={attribute.label}
+        value={value}
+        onChangeText={value => updateItem(attribute.id, value)}
+      />
+    );
+  };
+
+  const renderNumberAttribute = (attribute: IAttribute) => {
+    const value =
+      item.values.find(value => value.attribute === attribute.id)?.value || '';
+    return (
+      <TextInput
+        mode="outlined"
+        keyboardType="numeric"
+        label={attribute.label}
+        value={value}
+        onChangeText={value => updateItem(attribute.id, value)}
+      />
+    );
+  };
+
+  const renderCheckboxAttribute = (attribute: IAttribute) => {
+    const value =
+      item.values.find(value => value.attribute === attribute.id)?.value ||
+      false;
+    return (
+      <View style={styles.hStack}>
+        <Switch
+          value={value}
+          onValueChange={() => updateItem(attribute.id, !value)}
+        />
+        <Text variant="bodyLarge">{attribute.label}</Text>
+      </View>
+    );
+  };
+
+  const renderDateAttribute = (attribute: IAttribute) => {
+    const value =
+      item.values.find(value => value.attribute === attribute.id)?.value ||
+      undefined;
+    return (
+      <DatePicker
+        label={attribute.label}
+        value={value}
+        onChange={date => updateItem(attribute.id, date)}
+      />
+    );
+  };
+
   return (
     <Card style={styles.container} contentStyle={styles.content}>
       <Text variant="titleSmall">{`${title}`}</Text>
       {attributes.map(attribute => (
         <Fragment key={attribute.id}>
-          {attribute.type === 'Text' && (
-            <TextInput
-              mode="outlined"
-              label={attribute.label}
-              value={
-                item.values.find(value => value.attribute === attribute.id)
-                  ?.value || ''
-              }
-              onChangeText={value => updateItem(attribute.id, value)}
-            />
-          )}
-          {attribute.type === 'Number' && (
-            <TextInput
-              mode="outlined"
-              keyboardType="numeric"
-              label={attribute.label}
-              value={
-                item.values.find(value => value.attribute === attribute.id)
-                  ?.value || ''
-              }
-              onChangeText={value => updateItem(attribute.id, value)}
-            />
-          )}
-          {attribute.type === 'Checkbox' && (
-            <View style={styles.hStack}>
-              <Switch
-                value={
-                  !!item.values.find(value => value.attribute === attribute.id)
-                    ?.value
-                }
-                onValueChange={() =>
-                  updateItem(
-                    attribute.id,
-                    !!!item.values.find(
-                      value => value.attribute === attribute.id,
-                    )?.value,
-                  )
-                }
-              />
-              <Text variant="bodyLarge">{attribute.label}</Text>
-            </View>
-          )}
-
-          {attribute.type === 'Date' && (
-            <DatePicker
-              label={attribute.label}
-              value={
-                item.values.find(value => value.attribute === attribute.id)
-                  ?.value || undefined
-              }
-              onChange={date => updateItem(attribute.id, date)}
-            />
-          )}
+          {attribute.type === 'Text' && renderTextAttribute(attribute)}
+          {attribute.type === 'Number' && renderNumberAttribute(attribute)}
+          {attribute.type === 'Checkbox' && renderCheckboxAttribute(attribute)}
+          {attribute.type === 'Date' && renderDateAttribute(attribute)}
         </Fragment>
       ))}
       <Button
